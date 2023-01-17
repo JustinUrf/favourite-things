ddc7624d3bb8b1fd2001225ed36f1b9a01b30111
Justin Lee <lee.justin001126@gmail.com>
2023-01-17 14:16:15 -0800

    add for loops that iterate through the array's length and makes new elements

--- js/scripts.js
+++ js/scripts.js
@@ -7,19 +7,15 @@ function favouriteFoodList(event) {
   const favouriteFood2 = document.getElementById("food2").value;
   const favouriteFood3 = document.getElementById("food3").value;
   const favouriteFood4 = document.getElementById("food4").value;
-  let favouriteFoodArr = [favouriteFood1, favouriteFood2, favouriteFood3, favouriteFood4]
-  console.log(favouriteFoodArr)
+  let favouriteFoodArr = [favouriteFood1, favouriteFood2, favouriteFood3, favouriteFood4,]
   
-  let liElement = document.createElement("li");
   const olElement = document.createElement("ol");
   const form1 = document.querySelector("form")
   form1.after(olElement);
 
-
   for (let i = 0; i <= favouriteFoodArr.length-1; i++) {
-    console.log(i);
-    liElement;
-    liElement.textContent = favouriteFoodArr[i];
+    let liElement = document.createElement("li");
+    liElement.textContent = favouriteFoodArr[i]
     olElement.appendChild(liElement);
   }
 
